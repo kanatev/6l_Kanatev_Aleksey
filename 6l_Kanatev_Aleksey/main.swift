f7df4fd1f19f3fd46196aff67c0f0723237b235d
kanatev <aleksey.kanatev@mail.ru>
2018-12-10 12:00:25 +0500

    Initial Commit

---
+++ 6l_Kanatev_Aleksey/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  6l_Kanatev_Aleksey
+//
+//  Created by AlexMacPro on 10/12/2018.
+//  Copyright © 2018 AlexMacPro. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
